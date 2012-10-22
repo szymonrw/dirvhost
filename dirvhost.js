f4d45a7ab5f55ab1be77dfb6492d13f9058c49b9
Szymon Witamborski <szywon@live.com>
2012-10-22 15:33:07 +0200

    Init.

---
+++ dirvhost.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var connect = require('connect');
+var http    = require('http');
+var fs      = require('fs');
+
+var log_format = ':method :status :req[Host]:url'
+
+var app = connect().use(connect.logger(log_format));
+
+fs.readdir('.', function (err, list) {
+  list.forEach(function (dir_name) {
+    app.use(connect.vhost(dir_name + ".local", connect.static(dir_name)));
+  });
+
+  http.createServer(app).listen(4000);
+});

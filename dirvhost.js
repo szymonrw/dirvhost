79fc077d7e0d6bcdf0edbd21270e35e04ffca7ca
Szymon Witamborski <szywon@live.com>
2012-10-22 17:51:56 +0200

    Added watching directory for changes and directory listings.

--- dirvhost.js
+++ dirvhost.js
@@ -4,15 +4,49 @@
 var connect = require('connect');
 var http    = require('http');
 var fs      = require('fs');
+var path    = require('path');
 
-var log_format = ':method :status :req[Host]:url'
+var dir  = process.argv[2] || '.'
+var port = parseInt(process.argv[3], 10) || 4000;
 
-var app = connect().use(connect.logger(log_format));
+var log_format = ':method :status :req[Host]:url';
+var dir_view_options = { hidden: true, icons: true };
 
-fs.readdir('.', function (err, list) {
-  list.forEach(function (dir_name) {
-    app.use(connect.vhost(dir_name + ".local", connect.static(dir_name)));
+var app = connect()
+app.use(connect.logger(log_format));
+
+http.createServer(app).listen(port);
+
+var known_vhosts = {};
+
+function setup_vhost (dir_name) {
+  if (!known_vhosts[dir_name]) {
+    var domain = dir_name.replace(/ /g, '-') + ".local";
+    var dir_path = path.resolve(dir + '/' + dir_name);
+
+    var local_app = connect();
+    local_app.use(connect.static(dir_path));
+    local_app.use(connect.directory(dir_path));
+
+    app.use(connect.vhost(domain, local_app));
+    known_vhosts[dir_name] = true;
+
+    console.log('Serving ' + dir_path + ' as http://' + domain + ':' + port);
+  }
+}
+
+function refresh () {
+  fs.readdir(dir, function (err, list) {
+
+    if (err) {
+      console.error(err.stack || err);
+      process.exit(1);
+    }
+
+    list.forEach(setup_vhost);
   });
+}
 
-  http.createServer(app).listen(4000);
-});
+fs.watch(dir, refresh);
+
+refresh();
